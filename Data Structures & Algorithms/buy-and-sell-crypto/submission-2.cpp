4edbb48dc26fb3d72192587ee5d7fd5c16ff0756
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-04-25 11:49:54 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP =0;
+        int minB = prices[0];
+        for(int &sell:prices)
+        {
+            maxP = max(maxP,sell-minB);
+            minB = min(sell,minB);
+        }
+        return maxP;
+    }
+};

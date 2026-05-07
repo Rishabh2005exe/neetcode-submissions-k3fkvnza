c22ff3ffea9f7a3792cce2ccf499dab0b19c0610
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-05-07 14:09:18 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int>mp;
+
+        for(char c:t)
+            mp[c]++;
+        int l=0,count = t.size(), start=0;
+        int minLen = INT_MAX;
+
+        for(int r=0 ; r< s.size(); r++ )
+        {
+            if(mp[s[r]] > 0)
+                count--;
+            mp[s[r]]--;
+            while(count == 0)
+            {
+                if(r-l+1 <minLen)
+                {
+                    minLen = r-l+1;
+                    start=l;
+                }
+                mp[s[l]]++;
+
+                if(mp[s[l]]>0)
+                    count++;
+                l++;
+            }
+        }
+        if(minLen == INT_MAX)
+            return "";
+        return s.substr(start,minLen);
+    }
+};

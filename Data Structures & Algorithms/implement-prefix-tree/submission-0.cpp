4bbc6ffaefa03576d14f077e54e6d556b1a4ab84
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-05-24 13:56:31 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,51 @@
+class PrefixTree {
+
+public:
+    PrefixTree* children[26];
+    bool isLeaf;
+
+    PrefixTree() {
+        isLeaf = false;
+        for(int i=0;i<26;i++)
+            children[i] = nullptr;
+    }
+    
+    void insert(string word) {
+        PrefixTree* curr = this;
+        for(char c: word)
+        {
+            if(curr->children[c -'a'] == nullptr)
+            {
+                PrefixTree* newNode = new PrefixTree();
+
+                curr->children[c-'a'] = newNode;
+            }
+            curr = curr->children[c-'a'];
+        }
+        curr->isLeaf = true;
+    }
+    
+    bool search(string word) {
+        PrefixTree* curr=this;
+
+        for(char c:word)
+        {
+            if(curr->children[c-'a'] == nullptr)
+                return false;
+            curr = curr->children[c-'a'];
+        }
+        return curr->isLeaf;
+    }
+    
+    bool startsWith(string prefix) {
+        PrefixTree* current=this;
+        for(char c:prefix)
+        {
+            int index = c -'a';
+            if(current->children[index] == nullptr)
+                return false;
+            current = current->children[index];
+        }
+        return true;
+    }
+};

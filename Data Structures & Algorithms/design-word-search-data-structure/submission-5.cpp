384102321aa62d4f9569f40f1dbee2e74e214818
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-06-04 02:37:38 +0530

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.cpp
@@ -0,0 +1,63 @@
+class WordDictionary {
+    WordDictionary* children[26];
+    bool isLeaf;
+public:
+    WordDictionary() {
+        isLeaf = false;
+        for(int i =0;i<26;i++)
+            children[i] = nullptr;
+    }
+    
+    void addWord(string word) {
+        WordDictionary* curr = this;
+        for(char c:word)
+        {
+            int idx = c -'a';
+            if(curr->children[idx] == nullptr)
+                curr->children[idx] = new WordDictionary();
+            curr=curr->children[idx];
+        }
+        curr->isLeaf=true;
+    }
+    
+    bool dfs(string word , int pos , WordDictionary* node)
+    {
+        if(pos == word.size())
+            return node->isLeaf;
+        char c = word[pos];
+        if(c == '.')
+        {
+            for(int i=0 ;i<26;i++)
+            {
+                if(node->children[i] && dfs(word ,pos+1,node->children[i]))
+                    return true;
+            }
+            return false;
+        }
+        int idx = c -'a';
+        if(node->children[idx] == nullptr)
+            return false;
+        return dfs(word,pos+1,node->children[idx]);
+    }
+    bool search(string word) {
+        return dfs(word ,0,this);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

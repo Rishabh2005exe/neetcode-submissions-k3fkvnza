ffcb4ef4aa6e7f0fa7ce0ecc7d6853578e8b40d8
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-06-04 17:07:54 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void dfs(int idx , vector<int>& nums , vector<int>& curr)
+    {
+        if(idx == nums.size())
+        {
+            ans.push_back(curr);
+            return;
+        }
+        // include nums[idx]
+        curr.push_back(nums[idx]);
+        dfs(idx+1,nums,curr);
+        // Backtrack
+        curr.pop_back();
+
+        // exclude nums[idx]
+        dfs(idx +1,nums,curr);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
+        dfs(0,nums,curr);
+        return ans;
+    }
+};

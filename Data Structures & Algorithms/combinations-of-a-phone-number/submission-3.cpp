5344c430d81d7f0f7d8ac5c4f8d79050816981e3
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-06-09 13:50:38 +0530

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+        vector<string> ans;
+        void dfs(int index , string & current , string & digits , vector<string>& mp)
+        {
+            if(index == digits.size())
+            {
+                ans.push_back(current);
+                return;
+            }
+            string letters = mp[digits[index] - '0'];
+            for(char ch: letters)
+            {
+                current.push_back(ch);
+                dfs(index +1 , current , digits , mp);
+                current.pop_back();
+            }
+        }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        vector<string> mp={
+            "","","abc","def","ghi",
+            "jkl","mno","pqrs","tuv",
+            "wxyz"
+        };
+        string current = "";
+        dfs(0 , current , digits , mp);
+        return ans;
+    }
+};

9ebb69edb814e3f161d45669475f975571600019
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-06-10 12:38:10 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void dfs(int idx , vector<int>& nums , vector<int>& curr)
+    {
+        if(idx == nums.size())
+        {
+            ans.push_back(curr);
+            return;
+        }
+        // include nums[idx]
+        curr.push_back(nums[idx]);
+        dfs(idx +1 ,nums , curr);
+        curr.pop_back();
+
+        dfs(idx +1 , nums,curr);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>curr;
+        dfs(0,nums,curr);
+        return ans;
+    }
+};

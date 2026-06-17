3949e38f62310cb5e2d1522138d0f62a9eece5ef
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-06-17 12:56:57 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool dfs(int node , int parent , vector<vector<int>>& adj , vector<bool>& visited)
+    {
+        visited[node] = true;
+        for(int nei: adj[node])
+        {
+            if(!visited[nei])
+            {
+                if(!dfs(nei , node, adj , visited))
+                    return false;
+                
+            }
+            else if(nei != parent)
+                return false;
+        }
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size() != n-1)
+            return false;
+        vector<vector<int>> adj(n);
+
+        for(auto& e : edges)
+        {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<bool>visited(n,false);
+
+        if(!dfs(0,-1,adj,visited))
+            return false;
+        for(int i=0;i<n;i++)
+            if(!visited[i])
+                return false;
+        return true;
+    }
+};

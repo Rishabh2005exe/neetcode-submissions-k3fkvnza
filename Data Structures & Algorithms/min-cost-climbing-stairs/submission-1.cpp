b8206d022da8a6a802b984e9d4372a71f68ccff2
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-07-02 11:06:17 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public: 
+    vector<int> cache;
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        cache.resize(n,-1);
+        return min(dfs(cost,n,0) , dfs(cost,n,1));
+    }
+    int dfs(vector<int>& cost , int n , int i)
+    {
+        if(i>=n)
+            return 0;
+        if(cache[i] != -1)
+            return cache[i];
+        return cache[i] = cost[i]+ min(dfs(cost , n,i+1)  , dfs(cost ,n , i+2));
+    }
+};

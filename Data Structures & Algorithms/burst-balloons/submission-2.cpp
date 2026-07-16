4652a88c31709d7f8d06c744ec8cf9d39b8dd1d9
Rishabh2005exe <165818409+Rishabh2005exe@users.noreply.github.com>
2026-07-16 10:43:33 +0530

    Add: burst-balloons - submission-2

---
+++ Data Structures & Algorithms/burst-balloons/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        //add virtual ballons
+        nums.insert(nums.begin() , 1);
+        nums.push_back(1);
+
+        vector<vector<int>> dp( n+2 , vector<int>(n+2 ,-1));
+        return dfs(0, n+1 , nums,dp);
+    }
+    int dfs(int left , int right , vector<int>& nums , vector<vector<int>>& dp)
+    {
+        if(left +1  == right)
+            return 0;
+        if(dp[left][right] != -1)
+            return dp[left][right];
+        int ans =0;
+
+        // trying all balloons
+        for(int k = left +1 ; k< right ;k++)
+        {
+            int coins = nums[left]* nums[k]* nums[right]
+            + dfs(left , k, nums, dp)
+            + dfs(k , right , nums , dp);
+
+            ans = max(ans , coins);
+        }
+        return dp[left][right] = ans;
+    }
+};
